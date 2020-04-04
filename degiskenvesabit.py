3de44677e7e128f93dfa431a8bb9fb0dd8c3f28f
suleymanguven <34272909+suleymanguven@users.noreply.github.com>
2020-04-05 02:14:00 +0300

    Add files via upload

---
+++ degiskenvesabit.py
@@ -0,0 +1,49 @@
+#int(nteger): sayısal tam değer
+#str(string): metin değeri
+#float: kesirli sayısal değerler
+
+""""
+x=str(input("Adınızı Giriniz: "))
+y=str(input("Soyadınızı Giriniz"))
+
+print("Adınız Soyadınız: ",x," ",y)
+
+
+sayiDegeri=int(input("Sayı-1 değerini giriniz: "))
+sayi2=int(input("Sayı-2 değerini giriniz: "))
+toplam=sayiDegeri+sayi2
+print(toplam)
+
+pi=3
+r=float(input("Yarıçap değerini giriniz: "))
+alan=pi*r*r
+print(" Alan: ",alan)
+
+
+
+ad=str(input("Adınızı Giriniz: "))
+soyad=str(input("Soyadınızı Giriniz: "))
+not1=int(input("1. Sınav Notunuzu Giriniz: "))
+not2=int(input("2. Sınav Notunuzu Giriniz: "))
+perf=int(input("Performans Notunuzu Giriniz: "))
+proje=int(input("Proje Notunuzu Giriniz: "))
+ortalama=round(((not1+not2+perf+proje)/4),2)
+print("Sayın",ad,"",soyad,"Ortalamanız: ",ortalama)
+
+
+
+x=5
+y=6
+z=7
+print("x değeri= ",x)
+x1=int(input("x ile çarpılacak tam sayıyı giriniz: "))
+print("y değeri= ",y)
+y1=int(input("y ile çarpılacak tam sayıyı giriniz: "))
+print("z değeri= ",z)
+z1=float(input("z ile çarpılacak sayıyı giriniz: "))
+print("Çarpımlar: ")
+print("x ile çarpım sonucu: ",x*x1)
+print("y ile çarpım sonucu: ",y*y1)
+print("z ile çarpım sonucu: ",z*z1)
+
+"""
